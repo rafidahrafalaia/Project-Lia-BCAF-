f4f83dbee8178ff4b913ef39ed6a435a39dba2b3
rafidahrafalia <rafidahlia6@gmail.com>
2020-09-22 10:04:06 +0700

    done

---
+++ testing/src/main/java/com/bcafinance/rafidahlia/testing/Repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.bcafinance.rafidahlia.testing.Repository;
+
+import com.bcafinance.rafidahlia.testing.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
